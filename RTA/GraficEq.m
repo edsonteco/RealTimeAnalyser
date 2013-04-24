f64d318b3208fa07a452d16e3a5477036b9ae2d4
Edson Teco <edsonteco@gmail.com>
2013-04-24 10:45:27 -0300

    Linha inferior sempre visível com as bandas.

--- RTA/GraficEq.m
+++ RTA/GraficEq.m
@@ -33,9 +33,10 @@
     int banda, y, x = 0;
     int b0 = 0, b1;
     float peak;
+    float barWidth = self.frame.size.width/(self.bands);
     
     CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetLineWidth(context, (self.frame.size.width/self.bands)-((self.frame.size.width/self.bands)*0.15));
+    CGContextSetLineWidth(context, barWidth-(barWidth*0.15));
     [self.barColor setStroke];
     
     for (banda=0; banda<self.bands; banda++) {
@@ -44,16 +45,19 @@
         b1 = pow(2, banda*10.0/(self.bands-1));
         if (b1 > (FFT_LENGTH-1)) b1 = (FFT_LENGTH-1);
         if (b1 <= b0) b1 = b0 + 1;
-        for (; b0<b1; b0++) 
+        for (; b0<b1; b0++)
             if (peak < fft[1+b0]) peak = fft[1+b0];
         
         y = sqrt(peak) * 3 * self.frame.size.height-4; // scale it (sqrt to make low values more visible)
         if (y > self.frame.size.height) y = self.frame.size.height; // cap it
         
-        x = x + self.frame.size.width/self.bands;
-
+        x = x + barWidth;
+        
         CGContextMoveToPoint(context, x, self.frame.size.height);
         CGContextAddLineToPoint(context, x, self.frame.size.height - y);
+        CGContextStrokePath(context);
+        CGContextMoveToPoint(context, x, self.frame.size.height);
+        CGContextAddLineToPoint(context, x, self.frame.size.height - 2);
         CGContextStrokePath(context);
     }
 }
